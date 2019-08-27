bf7f8b26a8add63c6fadcf1707692c940cd6825c
MARIO REVEGLIA <mariocmp@live.it>
2019-08-27 13:00:09 +0200

    Formattazione

--- ClientApp/src/app/services/make.service.ts
+++ ClientApp/src/app/services/make.service.ts
@@ -1,15 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MakeService {
-makes;
-  constructor(private http:HttpClient) { }
+
+  makes;
+
+  constructor(private http: HttpClient) { }
 
   getMakes() {
     return this.http.get<any>('/api/makes');
-    };
+  }
 }
