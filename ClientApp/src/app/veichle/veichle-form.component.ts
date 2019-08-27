6461955f9b5bee49c25f15de89b63f09235c59b1
MARIO REVEGLIA <mariocmp@live.it>
2019-08-27 12:03:28 +0200

    Aggiunta Component NewVecihle

---
+++ ClientApp/src/app/veichle/veichle-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-veichle-form',
+  templateUrl: './veichle-form.component.html',
+  styleUrls: ['./veichle-form.component.css']
+})
+
+export class VeichleFormComponent {
+}
